b879c730292db8e9a3d03c166b0183568df04701
OM PRAKASH KANNAN <omprakash.kannan@rootquotient.com>
2023-08-16 00:52:04 +0530

    Implemented main bar design in time tracker section

--- Src/script.js
+++ Src/script.js
@@ -144,8 +144,99 @@ document.addEventListener("DOMContentLoaded", function() {
 
         commonSection.appendChild(mainSection);
 
+            // Time tracker section
+
+            var timeTrackerSection = document.createElement("div");
+            timeTrackerSection.classList.add("time-tracker-section");
+                var timeTrackerBar = document.createElement("div");
+                timeTrackerBar.classList.add("time-tracker-section__time-tracker-bar");
+                    var timeTrackerBarField = document.createElement("div");
+                    timeTrackerBarField.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-field");
+                        var timeTrackerDescription = document.createElement("div");
+                        timeTrackerDescription.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-field__description-field");
+                            var timeTrackerDescriptionBar = document.createElement("input");
+                            timeTrackerDescriptionBar.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-field__description-field__input");
+                            var timeTrackerDescriptionBarAttribute = {"type":"text","placeholder":"What are you working on?"};
+                            setCollection(timeTrackerDescriptionBar, timeTrackerDescriptionBarAttribute);
+                        var timeTrackerProject = document.createElement("div");
+                        timeTrackerProject.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-field__project");
+                            var timeTrackerProjectImg = document.createElement("div");
+                            timeTrackerProjectImg.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-field__project__img");
+                                var timeTrackerProjectAddImg = document.createElement("img");
+                                timeTrackerProjectAddImg.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-field__project__img__add-img");
+                                    var timeTrackerProjectAddImgAttribute = { "src":"./assets/Add newitem required field.svg", "alt":"Add-Image" };
+                                    setCollection(timeTrackerProjectAddImg, timeTrackerProjectAddImgAttribute);
+                            var timeTrackerProjectTxt = document.createElement("div");
+                            timeTrackerProjectTxt.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-field__project__txt");
+                                var timeTrackerProjectAddTxt = document.createElement("p");
+                                timeTrackerProjectAddTxt.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-field__project__txt__add-txt");
+                                    timeTrackerProjectAddTxt.innerHTML="Project";
+                    var timeTrackerBarButton = document.createElement("div");
+                    timeTrackerBarButton.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button");
+                        var timeTrackerTag = document.createElement("div");
+                        timeTrackerTag.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__tag-btn");
+                            var timeTrackerTagImg = document.createElement("img");
+                            timeTrackerTagImg.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__tag-btn__img");
+                                var timeTrackerTagImgAttribute = { "src":"./assets/Tag empty.svg", "alt":"Tag-Image" };
+                                setCollection(timeTrackerTagImg, timeTrackerTagImgAttribute);
+                        var timeTrackerBill = document.createElement("div");
+                        timeTrackerBill.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__bill-btn");
+                            var timeTrackerBillSymbol = document.createElement("p");
+                            timeTrackerBillSymbol.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__bill-btn__symbol");
+                            timeTrackerBillSymbol.innerHTML = "$";
+                        var timeTrackerAutoClock = document.createElement("div");
+                        timeTrackerAutoClock.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__clock-btn");
+                            var timeTrackerClock = document.createElement("div");
+                            timeTrackerClock.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__clock-btn__clock");
+                            timeTrackerClock.textContent = "00:00:00";
+                        var timeTrackerTrigger = document.createElement("div");
+                        timeTrackerTrigger.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__trigger-btn");
+                            var timeTrackerTriggerButton = document.createElement("button");
+                            timeTrackerTriggerButton.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__trigger-btn__btn");
+                            timeTrackerTriggerButton.innerHTML = "START";
+                        var timeTrackerMode = document.createElement("div");
+                        timeTrackerMode.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__mode");
+                            var timeTrackerAuto = document.createElement("div");
+                            timeTrackerAuto.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__mode__auto-btn");
+                                var timeTrackerAutoImg = document.createElement("img");
+                                timeTrackerAutoImg.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__mode__auto-btn__img");
+                                    var timeTrackerAutoImgAttribute = { "src":"./assets/Track time using timer selected.svg", "alt":"AutoClock-Image" };
+                                    setCollection(timeTrackerAutoImg, timeTrackerAutoImgAttribute);
+                            var timeTrackerManual = document.createElement("div");
+                            timeTrackerManual.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__mode__manual-btn");
+                                var timeTrackerManualImg = document.createElement("img");
+                                timeTrackerManualImg.classList.add("time-tracker-section__time-tracker-bar__time-tracker-bar-button__mode__manual-btn__img");
+                                    var timeTrackerManualImgAttribute = { "src":"./assets/Add time manually selected.svg", "alt":"AutoClock-Image" };
+                                    setCollection(timeTrackerManualImg, timeTrackerManualImgAttribute);
+            
+            mainSection.appendChild(timeTrackerSection);
+                timeTrackerSection.appendChild(timeTrackerBar);
+                    timeTrackerBar.appendChild(timeTrackerBarField);
+                        timeTrackerBarField.appendChild(timeTrackerDescription);
+                            timeTrackerDescription.appendChild(timeTrackerDescriptionBar);
+                        timeTrackerBarField.appendChild(timeTrackerProject);
+                            timeTrackerProject.appendChild(timeTrackerProjectImg);
+                                timeTrackerProjectImg.appendChild(timeTrackerProjectAddImg);
+                            timeTrackerProject.appendChild(timeTrackerProjectTxt);
+                                timeTrackerProjectTxt.appendChild(timeTrackerProjectAddTxt);
+                    timeTrackerBar.appendChild(timeTrackerBarButton);
+                        timeTrackerBarButton.appendChild(timeTrackerTag);
+                            timeTrackerTag.appendChild(timeTrackerTagImg)
+                        timeTrackerBarButton.appendChild(timeTrackerBill);
+                            timeTrackerBill.appendChild(timeTrackerBillSymbol);
+                        timeTrackerBarButton.appendChild(timeTrackerAutoClock);
+                            timeTrackerAutoClock.appendChild(timeTrackerClock);
+                        timeTrackerBarButton.appendChild(timeTrackerTrigger);
+                            timeTrackerTrigger.appendChild(timeTrackerTriggerButton)
+                        timeTrackerBarButton.appendChild(timeTrackerMode);
+                            timeTrackerMode.appendChild(timeTrackerAuto);
+                                timeTrackerAuto.appendChild(timeTrackerAutoImg);
+                            timeTrackerMode.appendChild(timeTrackerManual);
+                                timeTrackerManual.appendChild(timeTrackerManualImg);
+
 // Event listener
 
+// To change the visibility of the additional options in the sidebar alternatively
 show.addEventListener("click", function() {
     var additionalOptions = [kiosks, schedule, expenses, timeOff, activity, approvals, invoices];
     additionalOptions.forEach(option => option.classList.toggle("show-hidden"));
@@ -162,7 +253,7 @@ show.addEventListener("click", function() {
     show.classList.toggle("hidden");
 });
 
-
+// To change the size of the menu to icon and full context alternatively
 menuSizeImg.addEventListener("click", function() {
     
     var sidebarContext = document.querySelectorAll(".sidebar-section__main-content__option__context");
@@ -181,6 +272,17 @@ menuSizeImg.addEventListener("click", function() {
         item.classList.toggle("sidebar-icon-expand");
     })
     
+});
+
+// Mark the timesheet billable or not
+timeTrackerBillSymbol.addEventListener("click", function() {
+    var billSymbol = document.querySelector(".time-tracker-section__time-tracker-bar__time-tracker-bar-button__bill-btn__symbol");
+    if(billSymbol.classList.contains("billable")){
+        billSymbol.classList.toggle("billable");
+    }
+    else{
+        billSymbol.classList.toggle("billable");
+    }
 });
 
 
